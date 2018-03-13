6ac2fa20783e9e8f0de8261671a182e069f2b626
Ben Lancaster <bdl@live.co.uk>
2018-03-13 14:35:48 +0000

    core: impl: Button debouncing
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- prco_core/impl/xc6lx9_minispartanp.v
+++ prco_core/impl/xc6lx9_minispartanp.v
@@ -1,9 +1,14 @@
 // prco_core
 
+`define PORTC3_LGWAIT 16
+
 module xc6lx9_msp(
     input clk50,
     input [3:0] DIPSW,
     
+	// Button, Active HIGH
+	input PORTC3,
+	 
     // uart tx 
     output PORTC10,
     output PORTC11,
@@ -11,7 +16,7 @@ module xc6lx9_msp(
     output [7:0] LEDS
 );
 
-    reg     inst_core_enable = 1;
+	reg     inst_core_enable = 1;
     wire    q;
 
     wire    w_leds;
@@ -43,8 +48,61 @@ module xc6lx9_msp(
         end
     end
     
-    assign LEDS = { {5{1'b0}}, r_uart_q_tx, clk50, r_top_debug_instr_clk};
+    assign LEDS = { 
+		//{4{1'b0}}, 
+		test_bin,
+		test_state, 
+		r_uart_q_tx, 
+		clk50, 
+		r_top_debug_instr_clk
+	};
+	
     assign PORTC11 = r_uart_q_tx;
     assign PORTC10 = r_uart_i_rx;
+	 
+	// Debounce PORTC3 (instruction stepper)
+	reg test_state = 1'b0;
+	reg [3:0] test_bin = 4'b0001;
+	reg sync_pipe;
+	initial	sync_pipe      = 1'b0;
+	reg r_button_state;
+	initial	r_button_state = 1'b0;
+	reg [`PORTC3_LGWAIT-1:0] timer;
+	
+	reg r_last;
+	reg r_button_event;
+	initial	r_last         = 1'b0;
+	initial	r_button_event = 1'b0;
+	
+	always @(posedge clk50)
+		{ r_button_state, sync_pipe }
+			<= { sync_pipe, PORTC3 };
+	
+	initial timer = {(`PORTC3_LGWAIT){1'b1}};
+	reg o_debounced = 0;
+	always @(posedge clk50) begin
+		timer <= timer - 1'b1;
+		if(timer == 0) begin
+			o_debounced <= r_button_state;
+		end
+	end
+	
+	always @(posedge clk50)
+	begin
+		r_last <= o_debounced;
+		r_button_event <= (o_debounced)&&(!r_last);
+	end
+	
+	always @(posedge clk50) begin
+		if(r_button_event)
+			test_state <= !test_state;
+	end
+	
+	always @(posedge clk50)
+	begin
+		if(r_button_event) begin
+			test_bin <= test_bin + 1;
+		end
+	end
 
 endmodule
--- prco_core/rtl/test/tb_xc6lx9_minispartanp.v
+++ prco_core/rtl/test/tb_xc6lx9_minispartanp.v
@@ -5,12 +5,14 @@ module tb_xc6lx9_minispartanp;
     // Inputs
     reg clk50;
     reg [7:0] LEDS;
+	reg PORTC3;
 
     always #10 clk50 = ~clk50;
 
     // Instantiate the Unit Under Test (UUT)
     xc6lx9_msp uut (
-        .clk50(clk50), 
+        .clk50(clk50),
+		.PORTC3(PORTC3),		
         .LEDS(LEDS)
     );
 
@@ -18,15 +20,51 @@ module tb_xc6lx9_minispartanp;
         // Initialize Inputs
         clk50 = 0;
         LEDS = 0;
+		PORTC3 = 0;
         
         // Add stimulus here
         @(posedge clk50);
         @(posedge clk50);
         @(posedge clk50);
+		
+		PORTC3 = 1;
+        @(posedge clk50);
+		PORTC3 = 0;
+        @(posedge clk50);
+		PORTC3 = 1;
+        @(posedge clk50);
+		
+		#100;
+		
+		PORTC3 = 0;
+        @(posedge clk50);
+		PORTC3 = 1;
+        @(posedge clk50);
+		PORTC3 = 0;
         @(posedge clk50);
         @(posedge clk50);
         @(posedge clk50);
         @(posedge clk50);
+        @(posedge clk50);
+        @(posedge clk50);
+		
+		
+		#1000000
+		PORTC3 = 1;
+        @(posedge clk50);
+        @(posedge clk50);
+        @(posedge clk50);
+		PORTC3 = 0;
+        @(posedge clk50);
+        @(posedge clk50);
+        @(posedge clk50);
+		PORTC3 = 1;
+		
+		
+		
+		#1000000
+		PORTC3 = 0;
+		
     end
       
 endmodule
