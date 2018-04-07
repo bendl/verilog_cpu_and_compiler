248f6321d8c76879ba4f52ca05b9884077e496b2
Ben Lancaster <bdl@live.co.uk>
2018-04-07 18:44:15 +0100

    test: Add missing test runner script
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

---
+++ prco_compiler/test/travis-ci.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+num_tests=0
+passed_tests=0
+
+fnc_make() {
+    pushd ../
+
+    mkdir lbuild
+    cd lbuild
+    cmake ..
+    make
+
+    popd
+}
+
+fnc_run_cli() {
+    ../lbuild/cli/cli -i $1 -d -D0x0002
+}
+
+fnc_run_emu() {
+    ../lbuild/emu/emu -D0x0000
+}
+
+fnc_run_test() {
+    echo -n -e "Running test \t$1..."
+    num_tests=$(($num_tests + 1))
+
+    fnc_run_cli $1
+    fnc_run_emu $1 $2
+
+    if [ $? -eq $2 ]; then 
+        echo -e "\t\tPASSED"
+        passed_tests=$(($passed_tests + 1))
+    else
+        echo -e "\t\tFAILED\t\tExpected $2, got $?"
+    fi
+}
+
+fnc_make
+fnc_run_test ./tests/binary_ops_1.prco 6
+fnc_run_test ./tests/binary_ops_2.prco 11
+#fnc_run_test ./tests/binary_ops_3.prco 31
+
+
+fnc_run_test ./tests/control_for_1.prco 5
+fnc_run_test ./tests/control_for_2.prco 1
+fnc_run_test ./tests/control_for_3.prco 1
+
+fnc_run_test ./tests/control_if_1.prco 10
+fnc_run_test ./tests/control_if_2.prco 2
+fnc_run_test ./tests/control_if_2.prco 32
+
+fnc_run_test ./tests/control_while_1.prco 5
+fnc_run_test ./tests/control_while_2.prco 5
+fnc_run_test ./tests/control_while_3.prco 5
+fnc_run_test ./tests/control_while_4.prco 8
+
+fnc_run_test ./tests/foo.prco 1
+
+fnc_run_test ./tests/funcs_1.prco 33
+fnc_run_test ./tests/funcs_2.prco 2
+
+fnc_run_test ./tests/ports_uart_1.prco 32
+
+fnc_run_test ./tests/strings_1.prco 2
+fnc_run_test ./tests/strings_2.prco 0
+fnc_run_test ./tests/strings_3.prco 1
+
+fnc_run_test ./tests/vars_1.prco 0
+fnc_run_test ./tests/vars_2.prco 3
+
+echo "$passed_tests/$num_tests passed."
